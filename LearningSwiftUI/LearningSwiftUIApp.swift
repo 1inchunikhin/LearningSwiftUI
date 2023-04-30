a8d5ef38cda4e60f07eed82987e0c924c93e0c5a
1inchunikhin <1inchunikhin@gmail.com>
2023-04-30 23:58:37 +0300

    Initial Commit

---
+++ LearningSwiftUI/LearningSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningSwiftUIApp.swift
+//  LearningSwiftUI
+//
+//  Created by Николай Чунихин on 30.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct LearningSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
