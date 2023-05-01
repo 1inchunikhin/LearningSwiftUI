3e337c4aa8dc1fafd11559e406b6a32ede16efcd
1inchunikhin <1inchunikhin@gmail.com>
2023-05-01 10:00:23 +0300

    add toggles and view

--- LearningSwiftUI/ContentView.swift
+++ LearningSwiftUI/ContentView.swift
@@ -8,14 +8,56 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var toggleX = false
+    @State var toggleY = false
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        ZStack {
+            HStack{
+                VStack{
+                    HStack {
+                        Text("🖕")
+                            .font(.system(size: 70))
+                        Spacer()
+                    }
+                    Spacer()
+                    HStack {
+                        Spacer()
+                        Text("♥️")
+                            .font(.system(size: 75))
+                        Spacer()
+                    }
+                    .padding(100)
+                }
+                Spacer()
+            }
+            .padding(20)
+            
+            VStack {
+                
+                ZStack {
+                    RoundedRectangle(cornerRadius: 25)
+                        .fill(.red)
+                    Text("View")
+                        .font(.system(size: 32,
+                                      weight: .heavy,
+                                      design: .rounded))
+                }
+                .offset(x: toggleX ? 70 : 0)
+                .offset(y: toggleY ? -100 : 0)
+                .animation(.default, value: toggleY)
+                .animation(.default, value: toggleX)
+                
+                
+                Toggle("2nd SwiftUILesson X", isOn: $toggleX)
+                Toggle(isOn: $toggleY) {
+                    Text("2nd SwiftUILesson Y")
+                }
+                
+            }
+            .padding()
         }
-        .padding()
     }
 }
 
