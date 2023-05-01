ec20562f4e89cfd876918440511ed1a90735372a
1inchunikhin <1inchunikhin@gmail.com>
2023-05-01 11:18:12 +0300

    add NavigationView with Form including pickers

--- LearningSwiftUI/ContentView.swift
+++ LearningSwiftUI/ContentView.swift
@@ -8,14 +8,38 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var sectionTime = 0
+    @State var sectionMode = 0
+    @State var isOn = false
+    var pickerData = ["5 min", "10 min", "15 min", "20 min"]
+    var modes = ["Light", "Dark"]
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationView {
+            Form {
+                Picker("Time", selection: $sectionTime) {
+                    ForEach(0..<pickerData.count) {
+                        Text(self.pickerData[$0])
+                    }
+                }
+                .pickerStyle(.navigationLink)
+                
+                Toggle(isOn: $isOn) {
+                    Text("Plane mode")
+                        .foregroundColor(isOn ? .green : .orange)
+                }
+                
+                Picker("Mode", selection: $sectionMode) {
+                    ForEach(0..<modes.count) {
+                        Text(self.modes[$0])
+                    }
+                }
+                .pickerStyle(.segmented)
+                
+            }
+            .navigationBarTitle("Settings")
         }
-        .padding()
     }
 }
 
@@ -24,3 +48,4 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
